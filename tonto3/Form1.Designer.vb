43d984ed50c89528414b0db5ab844218dc280a39
Michael Vargas Soto <michaelvargas47@hotmail.com>
2021-10-13 09:35:21 -0600

    Add project files.

---
+++ tonto3/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.pic4 = New System.Windows.Forms.PictureBox()
+        Me.pic3 = New System.Windows.Forms.PictureBox()
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        Me.pic5 = New System.Windows.Forms.PictureBox()
+        Me.pic6 = New System.Windows.Forms.PictureBox()
+        Me.pic7 = New System.Windows.Forms.PictureBox()
+        Me.pic8 = New System.Windows.Forms.PictureBox()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.btnRevolver = New System.Windows.Forms.Button()
+        Me.btnParejas = New System.Windows.Forms.Button()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pic1
+        '
+        Me.pic1.Location = New System.Drawing.Point(12, 12)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(213, 189)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 0
+        Me.pic1.TabStop = False
+        '
+        'pic4
+        '
+        Me.pic4.Location = New System.Drawing.Point(830, 12)
+        Me.pic4.Name = "pic4"
+        Me.pic4.Size = New System.Drawing.Size(213, 189)
+        Me.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic4.TabIndex = 1
+        Me.pic4.TabStop = False
+        '
+        'pic3
+        '
+        Me.pic3.Location = New System.Drawing.Point(554, 12)
+        Me.pic3.Name = "pic3"
+        Me.pic3.Size = New System.Drawing.Size(213, 189)
+        Me.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic3.TabIndex = 2
+        Me.pic3.TabStop = False
+        '
+        'pic2
+        '
+        Me.pic2.Location = New System.Drawing.Point(282, 12)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(213, 189)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic2.TabIndex = 3
+        Me.pic2.TabStop = False
+        '
+        'pic5
+        '
+        Me.pic5.Location = New System.Drawing.Point(12, 297)
+        Me.pic5.Name = "pic5"
+        Me.pic5.Size = New System.Drawing.Size(213, 189)
+        Me.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic5.TabIndex = 4
+        Me.pic5.TabStop = False
+        '
+        'pic6
+        '
+        Me.pic6.Location = New System.Drawing.Point(282, 297)
+        Me.pic6.Name = "pic6"
+        Me.pic6.Size = New System.Drawing.Size(213, 189)
+        Me.pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic6.TabIndex = 5
+        Me.pic6.TabStop = False
+        '
+        'pic7
+        '
+        Me.pic7.Location = New System.Drawing.Point(554, 297)
+        Me.pic7.Name = "pic7"
+        Me.pic7.Size = New System.Drawing.Size(213, 189)
+        Me.pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic7.TabIndex = 6
+        Me.pic7.TabStop = False
+        '
+        'pic8
+        '
+        Me.pic8.Location = New System.Drawing.Point(830, 297)
+        Me.pic8.Name = "pic8"
+        Me.pic8.Size = New System.Drawing.Size(213, 189)
+        Me.pic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic8.TabIndex = 7
+        Me.pic8.TabStop = False
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(31, 220)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(12, 17)
+        Me.lbl1.TabIndex = 8
+        Me.lbl1.Text = "."
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(31, 514)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(12, 17)
+        Me.lbl2.TabIndex = 9
+        Me.lbl2.Text = "."
+        '
+        'btnRevolver
+        '
+        Me.btnRevolver.Location = New System.Drawing.Point(235, 548)
+        Me.btnRevolver.Name = "btnRevolver"
+        Me.btnRevolver.Size = New System.Drawing.Size(139, 67)
+        Me.btnRevolver.TabIndex = 10
+        Me.btnRevolver.Text = "Revovler"
+        Me.btnRevolver.UseVisualStyleBackColor = True
+        '
+        'btnParejas
+        '
+        Me.btnParejas.Location = New System.Drawing.Point(590, 548)
+        Me.btnParejas.Name = "btnParejas"
+        Me.btnParejas.Size = New System.Drawing.Size(139, 67)
+        Me.btnParejas.TabIndex = 11
+        Me.btnParejas.Text = "Parejas"
+        Me.btnParejas.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1072, 676)
+        Me.Controls.Add(Me.btnParejas)
+        Me.Controls.Add(Me.btnRevolver)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.pic8)
+        Me.Controls.Add(Me.pic7)
+        Me.Controls.Add(Me.pic6)
+        Me.Controls.Add(Me.pic5)
+        Me.Controls.Add(Me.pic2)
+        Me.Controls.Add(Me.pic3)
+        Me.Controls.Add(Me.pic4)
+        Me.Controls.Add(Me.pic1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents pic4 As PictureBox
+    Friend WithEvents pic3 As PictureBox
+    Friend WithEvents pic2 As PictureBox
+    Friend WithEvents pic5 As PictureBox
+    Friend WithEvents pic6 As PictureBox
+    Friend WithEvents pic7 As PictureBox
+    Friend WithEvents pic8 As PictureBox
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents btnRevolver As Button
+    Friend WithEvents btnParejas As Button
+End Class
---
+++ tonto3/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+#Region "Variables"
+    Dim vec1() As Integer
+    Dim vect2() As Integer
+
+#End Region
+
+#Region "Metodos"
+
+    '//Asignamos numeros aleatorios a las picture box y subimos las imagenes
+    Sub NumerosAleatorios(vector1() As Integer, lblInfoNumeros As Label, pic0 As PictureBox, pic1 As PictureBox,
+                      pic2 As PictureBox, pic3 As PictureBox)
+
+        Dim numeroAleatorio, contadorNumerosAleatorios As Integer
+        Dim validaNumero As Boolean
+
+        lblInfoNumeros.Text = ""
+        vector1 = {0, 0, 0, 0}
+        validaNumero = False
+        contadorNumerosAleatorios = 0
+
+        Do While (contadorNumerosAleatorios < 4)
+            Randomize()
+            numeroAleatorio = (CInt(Rnd() * 41)) + 1
+
+            For i = 0 To 3 Step 1
+                If vector1(i) = numeroAleatorio Then
+                    validaNumero = True
+                    Exit For
+                Else
+                    validaNumero = False
+
+                End If
+            Next
+
+            If validaNumero = False Then
+                For j = 0 To 3 Step 1
+                    If vector1(j) = 0 Then
+                        vector1(j) = numeroAleatorio
+                        contadorNumerosAleatorios = contadorNumerosAleatorios + 1
+                        Exit For
+                    End If
+                Next
+            End If
+
+        Loop
+
+        For k = 0 To 3 Step 1
+            lblInfoNumeros.Text = lblInfoNumeros.Text + Str(vector1(k))
+            Select Case k
+                Case 0
+
+                    pic0.ImageLocation = "C:\Users\micha\source\repos\tonto3\tonto3\Imagenes\a" + Str(vector1(k)) + ".jpg"
+                Case 1
+                    pic1.ImageLocation = "C:\Users\micha\source\repos\tonto3\tonto3\Imagenes\a" + Str(vector1(k)) + ".jpg"
+                Case 2
+                    pic2.ImageLocation = "C:\Users\micha\source\repos\tonto3\tonto3\Imagenes\a" + Str(vector1(k)) + ".jpg"
+                Case 3
+                    pic3.ImageLocation = "C:\Users\micha\source\repos\tonto3\tonto3\Imagenes\a" + Str(vector1(k)) + ".jpg"
+            End Select
+        Next
+    End Sub '// Fin metodo NumerosAleatorios
+
+    Private Sub btnRevolver_Click(sender As Object, e As EventArgs) Handles btnRevolver.Click
+        NumerosAleatorios(vec1, lbl1, pic1, pic2, pic3, pic4)
+        NumerosAleatorios(vect2, lbl2, pic5, pic6, pic7, pic8)
+    End Sub
+
+    Private Sub btnParejas_Click(sender As Object, e As EventArgs) Handles btnParejas.Click
+
+    End Sub
+
+#End Region
+
+End Class '//fin class
